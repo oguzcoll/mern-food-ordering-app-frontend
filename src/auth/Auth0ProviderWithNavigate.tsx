ffcd97893c327fa07a3a4bb1685362f95c8e9348
oguzcoll <oguzcolak2000@gmail.com>
2024-03-09 12:39:09 +0300

    updated env

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const navigate = useNavigate();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-  const clientId = import.meta.env.VITE_AUTH0_CLIENDT_ID;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
   const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
